da773a2296afb8f670d9a460607320964d99cc18
johtela <tommi.johtela@dnainternet.net>
2021-08-02 11:29:32 +0300

    Replaced tippy.js with <100 lines of JS and CSS

--- components/tooltips/tooltips.ts
+++ components/tooltips/tooltips.ts
@@ -1,16 +1,33 @@
-import tippy from 'tippy.js'
+import * as $ from "../../components/common/myquery"
 import './tooltips.less'
 
 // let body = document.getElementsByTagName('body')[0]
-document.querySelectorAll('[data-toggle="tooltip"]').forEach(elem => {
-    tippy(elem, {
-        content: elem.getAttribute("data-title"),
-        placement: 'top',
-        maxWidth: window.innerWidth * 0.8,
-        arrow: true,
-        theme: 'custom',
-        boundary: "window",
-        delay: 500,
-        allowHTML: false
-    })
-})
+document.querySelectorAll('[data-toggle="tooltip"]').forEach(elem => 
+    tooltip(elem as HTMLElement, elem.getAttribute("data-title")))
+
+const id = "tooltip"
+
+export function tooltip(elem: HTMLElement, text: string) {
+    elem.addEventListener('mouseenter', () => showTooltip(elem, text))
+    elem.addEventListener('mouseleave', hideTooltip)
+}
+
+function showTooltip(elem: HTMLElement, contents?: string) {
+    hideTooltip()
+    if (!contents)
+        return
+    let tt = $.create('legend')
+    document.body.appendChild(tt)
+    tt.id = id
+    tt.innerHTML = contents
+    let bb = elem.getBoundingClientRect()
+    tt.style.left = `${Math.round(bb.left) + window.scrollX}px`
+    tt.style.top = `${Math.round(bb.top) + window.scrollY}px`
+    tt.style.opacity = "95%"
+}
+
+function hideTooltip() {
+    let tt = document.getElementById(id)
+    if (tt)
+        tt.remove()
+}
